2bda946a7d3563bf2c986b23e012f1946c039c17
Ilya <ilya.kechko@fmobile.by>
2024-10-25 18:06:36 +0300

    add comment

--- src/main/java/com/example/petproject/PetProjectApplication.java
+++ src/main/java/com/example/petproject/PetProjectApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetProjectApplication {
 
     public static void main(String[] args) {
+        //111
         SpringApplication.run(PetProjectApplication.class, args);
     }
 
